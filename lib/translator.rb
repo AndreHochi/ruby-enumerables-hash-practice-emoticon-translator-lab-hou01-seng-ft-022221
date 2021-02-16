e5f3b90ea6995dfd496b7518f9ca7e2114f63f40
Andre Hochi <andrehochiwork@gmail.com>
2021-02-16 02:54:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,34 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  hash = {}
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |meaning, emoticon|
+    hash[meaning] = {:english => emoticon[0], :japanese => emoticon[1]}
+  end
+  hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emote)
+  english = ""
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |meaning, emoticon|
+    if emoticon[1] == emote
+      english = emoicon[0]
+    end
+  end
+  english
 end
 
 def get_english_meaning
-  # code goes here
+  japanese = ""
+  emoticons = YAML.load_file(file_path)
+  emoticons.each do |meaning, emoticon|
+    if emoticon[0] == emote
+      japanese = emoicon[1]
+    end
+  end
+  japanese
 end
